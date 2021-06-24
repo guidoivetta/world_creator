470b8a765a85f915b27ad2d8f809ef00e30b6d73
teaecetyrannis <teaecetyrannis@hotmail.com>
2021-06-24 07:22:01 -0300

    Tile system additions

--- Typewriter.js
+++ Typewriter.js
@@ -2,26 +2,22 @@ class Typewriter {
 
   constructor() {
     this.x = 1;
-    this.y = MAP_H + 2;
+    this.y = VIEWPORT_H + 2;
   }
 
-  type(char, x, y, off) {
-    if (typeof char != 'string') {
-      image(char, (x+1)*FONT_W+off, (y+1)*FONT_H+off);
-      return;
-    }
+  type(texture, x, y) {
     fill(255);
-    text(char, (x + 1) * FONT_W, (y + 2) * FONT_H);
+    text(texture, (x + 1) * FONT_W, (y + 2) * FONT_H);
   }
 
   drawScreenBorders() {
-    for (let x = 0; x < MAP_W + 2; x++) {
+    for (let x = 0; x < VIEWPORT_W + 2; x++) {
       this.type('-', x - 1, -1, 255);
-      this.type('-', x - 1, MAP_H, 255);
+      this.type('-', x - 1, VIEWPORT_H, 255);
     }
-    for (let y = 0; y < MAP_H + 2; y++) {
+    for (let y = 0; y < VIEWPORT_H + 2; y++) {
       this.type('|', -1, y - 1, 255);
-      this.type('|', MAP_W, y - 1, 255);
+      this.type('|', VIEWPORT_W, y - 1, 255);
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 ///***GLOBAL CONSTANTS***///
 const FONT_W = 9;
 const FONT_H = 12;
-const MAP_W = 48;
-const MAP_H = 24;
+const MAP_W = 96;
+const MAP_H = 48;
+const VIEWPORT_W = 48;
+const VIEWPORT_H = 24;
 
 ///***GLOBAL VARIABLES***///
 
@@ -13,12 +15,50 @@ let font;
 let layers = {
   'back': [],
   'middle': [],
-  'front': []
+  'front': [],
+
+  updateable: {
+    'back': [],
+    'middle': [],
+    'front': []
+  }
 };
 let terrain = [];
 let structures = [];
-let items = [];
-let entities = [];
+// let items = [];
+// let entities = [];
+
+//Dictionaries
+let colDict;
+let textures;
+let tiles = {
+  'log': {
+    isCollidable: true,
+    type: 'breakable',
+    material: 'wooden',
+    update: function(index) {
+      if (layers.updateable['middle'][index].wear >= 100) {
+        layers.updateable['middle'][index] = null;
+        layers['middle'][index] = null;
+      }
+    }
+  },
+  'water': {
+    isCollidable: false,
+    type: 'liquid'
+  }
+
+};
+let materials = {
+  'wooden': {
+    hardness: 10
+  },
+  'stone': {
+    hardness: 15
+  }
+};
+
+//Player
 let player = {
   pos: {
     x: 0,
@@ -26,68 +66,119 @@ let player = {
   },
   speed: 1,
   facing: 'e',
+  inventory: [],
+
   moveAndCollide: function() {
     //Calculate target posiiton
     let targetPos = {
-      x: player.pos.x + player.vel.x,
-      y: player.pos.y + player.vel.y
+      x: this.pos.x + this.vel.x,
+      y: this.pos.y + this.vel.y
     }
+    
+    //Get target tile full name (type_material)
+    let targetTileIndex = (targetPos.y*MAP_W)+targetPos.x;
+    let targetTileName = layers['middle'][targetTileIndex];
 
-    let targetTileName = layers['middle'][(targetPos.y*MAP_W)+targetPos.x];
-    let targetTile = '';
+    //If it exists
     if (targetTileName) {
+      //Get target tile (type)
+      let targetTile = '';
       for (let i = 0; i < targetTileName.length; i++) {
         if (targetTileName[i] == '_') {
           break;
         }
         targetTile += targetTileName[i];
       }
+      //If tile is collidable
       if (tiles[targetTile].isCollidable) {
+        this.collideWith(targetTile, targetTileIndex);
         return;
       }
     }
 
-    player.pos.x = targetPos.x;
-    player.pos.y = targetPos.y;
+    //Move to target position
+    this.pos.x = targetPos.x;
+    this.pos.y = targetPos.y;
 
-    if (player.pos.x < 0) {
-      player.pos.x = 0;
+    //Limit position to map boundaries
+    if (this.pos.x < 0) {
+      this.pos.x = 0;
     }
-    if (player.pos.x > MAP_W-1) {
-      player.pos.x = MAP_W-1;
+    if (this.pos.x > MAP_W-1) {
+      this.pos.x = MAP_W-1;
     }
-    if (player.pos.y < 0) {
-      player.pos.y = 0;
+    if (this.pos.y < 0) {
+      this.pos.y = 0;
     }
-    if (player.pos.y > MAP_H-1) {
-      player.pos.y = MAP_H-1;
+    if (this.pos.y > MAP_H-1) {
+      this.pos.y = MAP_H-1;
+    }
+
+    //Adjust camera
+    cam.adjust();
+  },
+
+  collideWith: function(tile, index) {
+    //Initialize updateable tile if there isn't one yet
+    if (!layers.updateable['middle'][index]) {
+      switch (tiles[tile].type) {
+        case 'breakable':
+          print('collided for the first time');
+          layers.updateable['middle'][index] = {
+            wear: 0
+          }
+          break;
+        case 'collided for the first time and it is liquid':
+          print('is liquid');
+          break;
+      }
+    }
+    switch (tiles[tile].type) {
+      case 'breakable':
+        if (player.tool.type == 'axe' && (tile == 'log' || tile == 'wooden')) {
+          layers.updateable['middle'][index].wear += (materials[player.tool.material].hardness*2) - materials[tiles[tile].material].hardness;
+        }
+        layers.updateable['middle'][index].wear += 10;
+        print('collider is breakable and its wear is '+layers.updateable['middle'][index].wear);
+        break;
+      case 'liquid':
+        print('collider is liquid');
+        break;
+    }
+    
+    tiles[tile].update(index);
+  }
+};
+
+//Cam
+let cam = {
+  x: 0,
+  y: 0,
+  dragDist: 6,
+
+  adjust: function() {
+    if (player.pos.x < this.x+this.dragDist) {
+      this.x--;
+    }
+    if (player.pos.x-this.x >= VIEWPORT_W-this.dragDist) {
+      this.x++;
+    }
+    if (this.x < 0) {
+      this.x = 0;
+    }
+
+    if (player.pos.y < this.y+this.dragDist) {
+      this.y--;
+    }
+    if (player.pos.y-this.y >= VIEWPORT_H-this.dragDist) {
+      this.y++;
+    }
+    if (this.y < 0) {
+      this.y = 0;
     }
   }
 };
 
-//Dictionaries
-let colDict;
-let textures;
-let hardness = {
-  'oak': 10,
-  'wooden': 10,
-  'stone': 20,
-  'tin': 30,
-  'golden': 40
-};
-let tiles = {
-  'log': {
-    isCollidable: true
-  }
-};
-
-//HTML
-let selectLayer;
-let selectType;
-let selectMaterial;
-let selectProperty;
-let selectTool;
-let buttonExport;
 
 ///***PRELOAD***///
 function preload() {
@@ -129,17 +220,15 @@ function setup() {
     leaves_autumn: color(212, 91, 18),
     leaves_dark: color(128, 80, 212)
   };
-
-  //Create typewriter, builder and player
+  
+  makeAlphaTexture('entity_player');
+  
+  //Create typewriter
   typewriter = new Typewriter();
 
-  //Set up canvas and style stuff
-  createCanvas(FONT_W * (MAP_W + 2), FONT_H * round(MAP_H * 1.5));
-  noSmooth();
-
   //Add dirt to terrain
-  for (let x = 0; x < MAP_W; x++) {
-    for (let y = 0; y < MAP_H; y++) {
+  for (let y = 0; y < MAP_H; y++) {
+    for (let x = 0; x < MAP_W; x++) {
       terrain.push('soil_dirt');
     }
   }
@@ -149,11 +238,10 @@ function setup() {
     layers['back'].push(terrain[i]);
   }
   
-
   //Spawn tree structures
   for (let i = 0; i < 5; i++) {
-    let x = floor(random(MAP_W * 0.75) + MAP_W * 0.125);
-    let y = floor(random(MAP_H * 0.75) + MAP_H * 0.125);
+    let x = floor(random(MAP_W * 0.75) + (MAP_W*0.125));
+    let y = floor(random(MAP_H * 0.75) + (MAP_H*0.125));
     structures.push(new Tree(x, y, 'oak', 'green'));
   }
 
@@ -162,24 +250,17 @@ function setup() {
     structures[i].loadToMap();
   }
 
-  pendingUpdate = true;
+  //Create chunks
+
+
+  //Set up canvas and style stuff
+  createCanvas(FONT_W * round(VIEWPORT_W * 1.5), FONT_H * round(VIEWPORT_H * 1.5));
+  noSmooth();
 
 }
 
 ///***DRAW***///
 function draw() {
-
-  update();
-
-  //Print framerate
-  if (frameRate() < 100) {
-    //print(frameRate());
-  }
-
-}
-
-///***UPDATE***///
-function update() {
 
   //Clear screen
   background(0);
@@ -191,8 +272,8 @@ function update() {
   i = 0;
   for (let y = 0; y < MAP_H; y++) {
     for (let x = 0; x < MAP_W; x++) {
-      if (layers['back'][i]) {
-        image(textures[layers['back'][i]], (x+1)*FONT_W,(y+1)*FONT_H);
+      if (layers['back'][i] && x-cam.x >= 0 && x-cam.x < VIEWPORT_W && y-cam.y >= 0 && y-cam.y < VIEWPORT_H) {
+        image(textures[layers['back'][i]], (x+1-cam.x)*FONT_W, (y+1-cam.y)*FONT_H);
       }
       i++;
     }
@@ -202,8 +283,8 @@ function update() {
   i = 0;
   for (let y = 0; y < MAP_H; y++) {
     for (let x = 0; x < MAP_W; x++) {
-      if (layers['middle'][i]) {
-        image(textures[layers['middle'][i]], (x+1)*FONT_W,(y+1)*FONT_H);
+      if (layers['middle'][i] && x-cam.x >= 0 && x-cam.x < VIEWPORT_W && y-cam.y >= 0 && y-cam.y < VIEWPORT_H) {
+        image(textures[layers['middle'][i]], (x+1-cam.x)*FONT_W, (y+1-cam.y)*FONT_H);
       }
       i++;
     }
@@ -216,19 +297,27 @@ function update() {
   }
 
   //Show player
-  image(textures['entity_player'], (player.pos.x+1)*FONT_W+1,(player.pos.y+1)*FONT_H-2);
+  image(textures['entity_player'], (player.pos.x+1-cam.x)*FONT_W+1, (player.pos.y+1-cam.y)*FONT_H-2);
 
   //Show stuff in the front
   i = 0;
   for (let y = 0; y < MAP_H; y++) {
     for (let x = 0; x < MAP_W; x++) {
-      if (layers['front'][i]) {
-        image(textures[layers['front'][i]], (x+1)*FONT_W,(y+1)*FONT_H);
+      if (layers['front'][i] && x-cam.x >= 0 && x-cam.x < VIEWPORT_W && y-cam.y >= 0 && y-cam.y < VIEWPORT_H) {
+        image(textures[layers['front'][i]], (x+1-cam.x)*FONT_W, (y+1-cam.y)*FONT_H);
       }
       i++;
     }
   }
-  
+
+  //Show player behind front tiles
+  image(textures['entity_player_alpha'], (player.pos.x+1-cam.x)*FONT_W+1, (player.pos.y+1-cam.y)*FONT_H-2);
+
+  //Print framerate constantly
+  if (frameRate() < 100) {
+    //print(frameRate());
+  }
+
 }
 
 ///***MOUSE AND KEYBOARD***///
@@ -274,5 +363,24 @@ function keyPressed() {
     player.facing = 's';
     player.moveAndCollide();
   }
+  if (keyCode == 113) { //F2 key
+    print(frameRate());
+  }
 
+}
+
+function makeAlphaTexture(texture){
+  textures[texture].loadPixels();
+  let newTexture = texture+'_alpha';
+  textures[newTexture] = createImage(FONT_W, FONT_H);
+  textures[newTexture].loadPixels();
+  for (let i = 0; i < textures[newTexture].pixels.length*0.25; i++) {
+    if (textures[texture].pixels[(i*4)+3] == 255) {
+      textures[newTexture].pixels[(i*4)] = 255;
+      textures[newTexture].pixels[(i*4)+1] = 255;
+      textures[newTexture].pixels[(i*4)+2] = 255;
+      textures[newTexture].pixels[(i*4)+3] = 95;
+    }
+  }
+  textures[newTexture].updatePixels();
 }
